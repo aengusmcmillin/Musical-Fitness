53af077cd57384ca0f669b4a0921803c8581e3a1
Aengus McMillin <aengusmcmillin@gmail.com>
2011-12-16 11:01:24 -0800

    Initial Commit of Musical Fitness

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicalFitness::Application.config.secret_token = '587624e0b5cea6c284ad7371f774a558abba3fce4445798e5c8a7e23bd5093a3512481238241cc18dcb9ec411ef4ae81445795f410a92a351c3988b6c93a905e'
