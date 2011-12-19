7ef9dce1b4986cfe7c453c5a3629e0f1bff331cd
Aengus McMillin <aengusmcmillin@gmail.com>
2011-12-19 07:49:14 -0800

    Made some more style changes, I also added the create action to the Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,15 @@ class UsersController < ApplicationController
     @title = "Sign Up"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Musical Fitness!"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,7 +13,7 @@ describe UsersController do
     end
 
     it "displays the users profile" do
-      response.should render_template("users/show")
+      response.should render_template("show")
     end
 
     it "has the correct title" do
@@ -24,7 +24,7 @@ describe UsersController do
   describe "#new" do
     it "displays the signup page" do
       get :new
-      response.should render_template("users/new")
+      response.should render_template("new")
     end
 
     it "has the correct title" do
@@ -34,6 +34,48 @@ describe UsersController do
   end
 
   describe '#create' do
+    context 'success' do
+      before :each do
+        @attr = { :username => "New User", :email => "Example@example.com", :password => "password", :password_confirmation => "password" }
+      end
 
+      it 'creates a user' do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+
+      it 'redirects to the user show page' do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+
+      it 'displays a welcome message' do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to musical fitness/i
+      end
+    end
+
+    context 'failure' do
+      before :each do
+        @attr = { :username => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it 'does not create a user' do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it 'has the correct title' do
+        post :create, :user => @attr
+        assigns(:title).should == "Sign Up"
+      end
+
+      it 'reloads the signup page' do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
   end
 end
